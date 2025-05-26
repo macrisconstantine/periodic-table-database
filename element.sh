bb93248c149c2b8310a55f8ed0d1a548187e0525
Constantine Elias Macris <c.macris@acg.edu>
2025-05-26 16:27:43 -0400

    feat: add lots of logic to .sh file

--- element.sh
+++ element.sh
@@ -1,10 +1,16 @@
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 
 
-if [[ $1 == "" ]]
+if [[ $1 ]]
 then
-  echo "Please provide an element as an argument."
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+      ATOMIC_NUMBER_RESULT=$($PSQL "SELECT * FROM Elements LEFT OUTER JOIN Properties USING (atomic_number) WHERE atomic_number = $1")
+      echo "$ATOMIC_NUMBER_RESULT" | WHILE read ATOMIC_NUMBER BAR SYMBOL BAR ELEMENT_NAME BAR ATOMIC_MASS BAR 
+  else
+    echo "argument is a string"
+  fi
 else
-  echo ""
+  echo "Please provide an element as an argument."
 fi
