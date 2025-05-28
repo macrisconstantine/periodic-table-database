5c352d8a0d0b2fdf1270acb2a4463f615427e3b9
Constantine Elias Macris <c.macris@acg.edu>
2025-05-28 16:47:28 -0400

    feat: add lots of logic to .sh file

--- element.sh
+++ element.sh
@@ -7,11 +7,10 @@ then
   if [[ $1 =~ ^[0-9]+$ ]]
   then
       ATOMIC_NUMBER_RESULT=$($PSQL "SELECT * FROM Elements LEFT OUTER JOIN Properties USING (atomic_number) LEFT OUTER JOIN Types USING (type_id) WHERE atomic_number = $1")
-      echo "$ATOMIC_NUMBER_RESULT" | while read ATOMIC_NUMBER BAR SYMBOL BAR ELEMENT_NAME BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID BAR TYPE
+      echo "$ATOMIC_NUMBER_RESULT" | while read Blah Blah ATOMIC_NUMBER BAR SYMBOL BAR ELEMENT_NAME BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE
       do 
-        echo "The element with atomic number $1 is $ELEMENT_NAME ($SYMBOL). It's a $TYPE, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+        echo "The element with atomic number $1 is $ELEMENT_NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
       done
-      echo "$ATOMIC_NUMBER_RESULT"
   else
 
     echo "argument is a string"
